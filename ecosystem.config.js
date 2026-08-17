3e12fc3d9ee972c575d694b7a8144e42366e48b8
Uyenlun113 <thiuyen1132004@gmail.com>
2026-08-17 14:43:26 +0700

    chore: update application port from 3000 to 3005 in Nginx configuration and PM2 ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,14 +3,14 @@ module.exports = {
     {
       name: 'nipt-genetrust',
       script: 'node_modules/next/dist/bin/next',
-      args: 'start -p 3000',
+      args: 'start -p 3009',
       instances: 1,
       autorestart: true,
       watch: false,
       max_memory_restart: '1G',
       env: {
         NODE_ENV: 'production',
-        PORT: 3000
+        PORT: 3009
       }
     }
   ]
