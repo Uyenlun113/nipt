6f65f11a5cff3bf78a94f46af8289dbb6469e765
Uyenlun113 <thiuyen1132004@gmail.com>
2026-09-03 08:41:42 +0700

    feat: initialize root layout with Plus Jakarta Sans font and site metadata

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="vi" className={plusJakarta.variable} suppressHydrationWarning>
-      <head>
+      <head suppressHydrationWarning>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
